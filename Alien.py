95149ccf6369e958479e7ce0eec92713d7b43b86
Benjamin Zheng <54424912+ZhengBenjamin@users.noreply.github.com>
2024-06-12 16:25:50 -0400

    Updated map & movement logic

--- Alien.py
+++ Alien.py
@@ -3,17 +3,18 @@ import pygame
 import os
 class Alien:
   
-  def __init__(self, health, speed, reward, x, y, img):
+  def __init__(self, health, speed, reward, path, img):
     self.rect = pygame.Rect(0, 0, 64, 64) # Rectangular hitbox
-    self.health = health
-    self.speed = speed
-    self.reward = reward
-    self.x = x
-    self.y = y
-    self.width = 64
-    self.height = 64
-    self.img = img
-    self.velocity = [0, 0]
+    self.health = health # Health of the alien
+    self.speed = speed # Speed of the alien
+    self.reward = reward # Reward for killing the alien
+    self.x = path[0]
+    self.y = path[1] 
+    self.width = 32
+    self.height = 32 
+    self.img = img # Image of the alien
+    self.velocity = [0, 0] # Initial velocity
+    self.turn = 0 # Current turn in the path
 
   # Getter methods
   
@@ -26,35 +27,81 @@ class Alien:
   def getReward(self):
     return self.reward
   
-  # Movement methods
+  # Render methods
 
   def draw(self, window): # Draw the alien
     window.blit(pygame.transform.scale(self.img, (self.width, self.height)), (self.x, self.y))
 
   def update(self, window):
-    self.x += self.velocity[0]
-    self.y += self.velocity[1]
+    self.move(self.velocity[0], self.velocity[1])
     self.draw(window)
+
+  # Movement methods
+
+  def move(self, dx, dy):
+    self.rect.x += dx
+    self.rect.y += dy
+    
+  def moveRight(self):
+    self.velocity[0] = self.speed
+  
+  def moveLeft(self):
+    self.velocity[0] = -self.speed
+
+  def moveUp(self):
+    self.velocity[1] = -self.speed
+
+  def moveDown(self):
+    self.velocity[1] = self.speed
+
+  def path(self, path):
+    totTurns = len(path)
+    
+    while self.turn < totTurns:
+      if path[self.turn][2] == "left" or "right":
+        if self.x == path[self.turn][0]:
+          self.turn += 1
+          self.calcNextTurn(path, self.turn)
+      elif path[self.turn][2] == "up" or "down":
+        if self.y == path[self.turn][1]:
+          self.turn += 1
+          self.calcNextTurn(path, self.turn)
+      else: 
+        pass
+        
+  def calcNextTurn(self, path, turn):
+    match path[turn][2]:
+      case "right":
+        self.moveRight()
+      case "left":
+        self.moveLeft()
+      case "up":
+        self.moveUp()
+      case "down":
+        self.moveDown()
+
+    
+  
 
 
 # Alien subclasses
 
 class Slime(Alien):
-  def __init__(self, xPos, yPos):
-    super().__init__(5, 2, 10, xPos, yPos, pygame.image.load("assets/slime.png"))
+  def __init__(self, path):
+    super().__init__(5, 2, 10, path, pygame.image.load("assets/slime.png"))
 
 class BobaAlien(Alien):
-  def __init__(self, xPos, yPos):
-    super().__init__(10, 1, 20, xPos, yPos, pygame.image.load("assets/boba.png"))
+  def __init__(self, path):
+    super().__init__(10, 1, 20, path, pygame.image.load("assets/boba.png"))
 
 class BigDaddyBen(Alien):
-  def __init__(self, xPos, yPos):
-    super().__init__(20, 1, 40, xPos, yPos, pygame.image.load("assets/bigDaddyBen.png"))
+  def __init__(self, path):
+    super().__init__(20, 1, 40, path, pygame.image.load("assets/bigDaddyBen.png"))
 
 class SkateboardAlien(Alien):
-  def __init__(self, xPos, yPos):
-    super().__init__(15, 3, 30, xPos, yPos, pygame.image.load("assets/skateboardAlien.png"))
+  def __init__(self, path):
+    super().__init__(15, 3, 30, path, pygame.image.load("assets/skateboardAlien.png"))
 
 class Healer(Alien):
-  def __init__(self, xPos, yPos):
-    super().__init__(10, 1, 20, xPos, yPos, pygame.image.load("assets/healer.png"))
+  def __init__(self, path):
+    super().__init__(10, 1, 20, path, pygame.image.load("assets/healer.png"))
