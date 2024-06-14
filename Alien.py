68fc470cc5d92e8e41fd579e96d695d6692657e3
Benjamin Zheng <54424912+ZhengBenjamin@users.noreply.github.com>
2024-06-14 13:31:53 -0400

    Corrected alien movement logic + started spawning logic

--- Alien.py
+++ Alien.py
@@ -4,17 +4,19 @@ import os
 class Alien:
   
   def __init__(self, health, speed, reward, path, img):
-    self.rect = pygame.Rect(0, 0, 64, 64) # Rectangular hitbox
+    self.rect = pygame.Rect(0, 0, 32, 32) # Rectangular hitbox
     self.health = health # Health of the alien
     self.speed = speed # Speed of the alien
     self.reward = reward # Reward for killing the alien
-    self.x = path[0]
-    self.y = path[1] 
+    self.x = path[1][0]
+    self.y = path[1][1] 
     self.width = 32
     self.height = 32 
     self.img = img # Image of the alien
     self.velocity = [0, 0] # Initial velocity
     self.turn = 0 # Current turn in the path
+    self.path = path # Path of the alien
+    self.currentDirection = path[0][2] # Current direction of the alien
 
   # Getter methods
   
@@ -27,20 +29,24 @@ class Alien:
   def getReward(self):
     return self.reward
   
+  def getPath(self):
+    return self.path
+
   # Render methods
 
   def draw(self, window): # Draw the alien
     window.blit(pygame.transform.scale(self.img, (self.width, self.height)), (self.x, self.y))
 
   def update(self, window):
+    self.updatePath()
     self.move(self.velocity[0], self.velocity[1])
     self.draw(window)
 
   # Movement methods
 
   def move(self, dx, dy):
-    self.rect.x += dx
-    self.rect.y += dy
+    self.x += dx
+    self.y += dy
     
   def moveRight(self):
     self.velocity[0] = self.speed
@@ -54,33 +60,59 @@ class Alien:
   def moveDown(self):
     self.velocity[1] = self.speed
 
-  def path(self, path):
-    totTurns = len(path)
-    
-    while self.turn < totTurns:
-      if path[self.turn][2] == "left" or "right":
-        if self.x == path[self.turn][0]:
+  def updatePath(self):
+
+    # Starting Direction
+    if self.turn == 0:
+      match self.path[0][2]:
+        case "right":
+          self.moveRight()
+        case "left":
+          self.moveLeft()
+        case "up":
+          self.moveUp()
+        case "down":
+          self.moveDown()
+        case _:
+          pass
+      self.turn += 1
+
+    # Remaining Paths
+    if self.turn < len(self.path):
+      print(self.turn, self.currentDirection)
+      if self.currentDirection in ["left", "right"]:
+        if self.x == self.path[self.turn][0]:
+          print("Turn index = " + str(self.turn) + " x: " + str(self.x))
+          self.calcNextPath()
           self.turn += 1
-          self.calcNextTurn(path, self.turn)
-      elif path[self.turn][2] == "up" or "down":
-        if self.y == path[self.turn][1]:
+      elif self.currentDirection in ["up", "down"]:
+        print("dected down")
+        if self.y == self.path[self.turn][1]:
+          print("Turn index = " + str(self.turn) + " y: " + str(self.y))
+          self.calcNextPath()
           self.turn += 1
-          self.calcNextTurn(path, self.turn)
-      else: 
-        pass
-        
-  def calcNextTurn(self, path, turn):
-    match path[turn][2]:
+
+  def calcNextPath(self):
+    self.velocity = [0,0]
+    nextDirection = self.path[self.turn][2]
+    match nextDirection:
       case "right":
         self.moveRight()
+        self.currentDirection = "right"
+        print("Moving right " + str(self.currentDirection))
       case "left":
         self.moveLeft()
+        self.currentDirection = "left"
+        print("Moving left"+ str(self.currentDirection))
       case "up":
         self.moveUp()
+        self.currentDirection = "up"
+        print("Moving up"+ str(self.currentDirection))
       case "down":
         self.moveDown()
-
-    
+        self.currentDirection = "down"
+        print("Moving down"+ str(self.currentDirection))
+    return
   
 
 
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import os
+import random
 from Alien import *
 from Tower import *
 from Shop import *
@@ -11,11 +12,12 @@ pygame.display.set_caption("Tower Defense") # Title of window
 WIDTH, HEIGHT = 1000, 800 # Window Size
 FPS = 120 # Frames per second
 
-level = [] # List of all levels
+maps = [] # List of all maps
 towers = [] # List of all towers TODO: Make sure to append towers to this list
 aliens = [] # List of all aliens TODO: Make sure to append aliens to this list
 
-level.append([0, 100, "right"], [400, 100, "down"], [400, 500, "right"], [800, 500, "up"], [800, 0, "end"])
+# Level layout: [[start], [verticies]]: [[startingDirection], [x, y, nextDirection]]
+maps.append([[200, 200, "right"], [400, 200, "down"], [400, 600, "right"], [800, 600, "up"]])
 
 window = pygame.display.set_mode((WIDTH, HEIGHT)) # Window
 
@@ -23,7 +25,7 @@ window = pygame.display.set_mode((WIDTH, HEIGHT)) # Window
 def draw():
   for alien in aliens:
     alien.update(window) 
- 
+
   pygame.display.update() # Update the window
 
 
@@ -44,9 +46,23 @@ def main(window):
     
     draw()
 
-# Testing
-testObject = Slime(40, 40)
-aliens.append(testObject)
+# Spawning aliens
+
+def startSpawn(level):
+  delay = 1000 # Delay between each alien spawn
+  timer = 0 
+
+  # Easy aliens 
+  if level < 3: 
+    for i in range(4):
+      if pygame.time.get_ticks() - timer > delay:
+        aliens.append(Slime(maps[0]))
+        timer = pygame.time.get_ticks()
+
+  # Med aliens 
+
+  # Hard aliens 
 
 main(window)
+startSpawn(1)
 
