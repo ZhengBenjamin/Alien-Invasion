046c637d7b383dd62f94969060d7ae81bd514b58
Benjamin Zheng <54424912+ZhengBenjamin@users.noreply.github.com>
2024-06-16 15:07:44 -0400

    Converted List items into Sprite Groups

--- main.py
+++ main.py
@@ -12,29 +12,27 @@ pygame.display.set_caption("Tower Defense") # Title of window
 # Global variables
 WIDTH, HEIGHT = 1000, 800 # Window Size
 FPS = 120 # Frames per second
-
-maps = [] # List of all maps
-towers = [] # List of all towers TODO: Make sure to append towers to this list
-aliens = [] # List of all aliens TODO: Make sure to append aliens to this list
-projectiles = [] # List of all projectiles TODO: Make sure to append projectiles to this list
-
-# Level layout: [[start], [verticies]]: [[startingDirection], [x, y, nextDirection]]
-maps.append([[100, 200, "right"], [400, 200, "down"], [400, 600, "right"], [800, 600, "up"]])
-
 window = pygame.display.set_mode((WIDTH, HEIGHT)) # Window
 
-# This method draws the window; Call draw for all objects here
-def draw():
-  for alien in aliens:
-    alien.update(window) 
+# Sprite Groups
+towers = pygame.sprite.Group() 
+aliens = pygame.sprite.Group()
+projectiles = pygame.sprite.Group() 
 
-  for projectile in projectiles:
-    projectile.update(window)
+# Maps
+# Level layout: [[start], [verticies]]: [[startingDirection], [x, y, nextDirection]]
+maps = [] # List of all maps
+maps.append([[200, 200, "right"], [400, 200, "down"], [400, 600, "right"], [800, 600, "up"]])
+
+# Draws the window and updates the sprites
+def draw():
+  aliens.update(window)
+  projectiles.update(window)
 
   pygame.display.update() # Update the window
 
 
-# This main method contains the loop of the game
+# Main game loop
 def main(window):
   clock = pygame.time.Clock()
 
@@ -49,27 +47,15 @@ def main(window):
         run = False
         break
     
-    #startSpawn(1)
     draw()
 
 # Spawning aliens
 
 def startSpawn(level):
-  time = 1000
-  nextTime = 0
-  # Easy aliens
+  pass
 
-  currTime = pygame.time.get_ticks()
-  if currTime > nextTime:
-    nextTime += time
-    aliens.append(Slime(maps[0]))
-
-  # Med aliens 
-
-  # Hard aliens 
-
-aliens.append(Slime(maps[0]))
-projectiles.append(CannonProj(50, 50, aliens[0]))
+aliens.add(Slime(maps[0]))
+projectiles.add(CannonProj(500, 500, aliens.sprites()[0]))
 main(window)
 
 
